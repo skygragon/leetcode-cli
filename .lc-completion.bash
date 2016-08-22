6a9554175a1651779e332e1052e66ae2f1381753
Eric Wang <skygragon@gmail.com>
2016-08-22 12:22:02 +0800

    Add tab completion for bash.
    Signed-off-by: Eric Wang <skygragon@gmail.com>

---
+++ .lc-completion.bash
@@ -0,0 +1,28 @@
+###-begin-lc-completions-###
+#
+# yargs command completion script
+#
+# Installation: bin/lc completion >> ~/.bashrc
+#    or bin/lc completion >> ~/.bash_profile on OSX.
+#
+_yargs_completions()
+{
+    local cur_word args type_list
+
+    cur_word="${COMP_WORDS[COMP_CWORD]}"
+    args=("${COMP_WORDS[@]}")
+
+    # ask yargs to generate completions.
+    type_list=$(lc --get-yargs-completions "${args[@]}")
+
+    COMPREPLY=( $(compgen -W "${type_list}" -- ${cur_word}) )
+
+    # if no match was found, fall back to filename completion
+    if [ ${#COMPREPLY[@]} -eq 0 ]; then
+      COMPREPLY=( $(compgen -f -- "${cur_word}" ) )
+    fi
+
+    return 0
+}
+complete -F _yargs_completions lc
+###-end-lc-completions-###
