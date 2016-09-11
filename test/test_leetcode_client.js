27037b8f8186274dce846e627040985d77924745
Eric Wang <skygragon@gmail.com>
2016-09-11 11:20:15 +0800

    Add UT for leetcode_client.getProblems
    Signed-off-by: Eric Wang <skygragon@gmail.com>

---
+++ test/test_leetcode_client.js
@@ -0,0 +1,21 @@
+var assert = require('chai').assert;
+var nock = require('nock');
+
+var client = require('../lib/leetcode_client');
+var config = require('../lib/config');
+
+describe('leetcode_client', function() {
+  describe('#getProblems', function() {
+    it('should ok', function(done) {
+      nock(config.PROBLEMS_URL)
+        .get('/')
+        .replyWithFile(200, './test/mock/problems.json');
+
+      client.getProblems(function(e, problems) {
+        assert.equal(e, null);
+        assert.equal(problems.length, 382);
+        done();
+      });
+    });
+  }); // #getProblems
+});
