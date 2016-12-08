01f54bd75853bfc9d47cc96c008951e91de04f39
Eric Wang <skygragon@gmail.com>
2016-12-08 10:56:39 +0800

    fixes #14: show error details in 'test'
    Signed-off-by: Eric Wang <skygragon@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
       "key-spacing": [2, { "align": "value" }],
       "max-len": [1, 120],
       "no-eval": 1, // we use it on purpose
+      "no-loop-func": 1,
       "no-multi-spaces": [2, { exceptions: { "SwitchCase": true }}],
       "no-unused-expressions": 1,
       "no-unused-vars": 1,
