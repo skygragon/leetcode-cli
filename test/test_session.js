a403ca01615671c5eab4f97f473f1018b16a7f58
Eric Wang <skygragon@gmail.com>
2018-01-04 12:30:37 +0800

    Show reasonable AC count in `stat -c`.
    Signed-off-by: Eric Wang <skygragon@gmail.com>

--- test/test_session.js
+++ test/test_session.js
@@ -23,18 +23,18 @@ describe('session', function() {
     session.__set__('moment', moment);
   });
 
+  beforeEach(function() {
+    stats = null;
+  });
+
   describe('#updateStat', function() {
-    it('should ok', function() {
+    it('should update number ok', function() {
       now = '2017.12.13';
       session.updateStat('ac', 10);
-      assert.deepEqual(stats, {
-        '2017.12.13': {ac: 10}
-      });
+      assert.deepEqual(stats, {'2017.12.13': {ac: 10}});
 
       session.updateStat('ac', 20);
-      assert.deepEqual(stats, {
-        '2017.12.13': {ac: 30}
-      });
+      assert.deepEqual(stats, {'2017.12.13': {ac: 30}});
 
       now = '2017.12.14';
       session.updateStat('ac', 40);
@@ -42,6 +42,16 @@ describe('session', function() {
         '2017.12.13': {ac: 30},
         '2017.12.14': {ac: 40}
       });
+    });
+
+    it('should update set ok', function() {
+      now = '2017.12.13';
+      session.updateStat('ac.set', 101);
+      assert.deepEqual(stats, {'2017.12.13': {'ac.set': [101]}});
+      session.updateStat('ac.set', 100);
+      assert.deepEqual(stats, {'2017.12.13': {'ac.set': [101, 100]}});
+      session.updateStat('ac.set', 101);
+      assert.deepEqual(stats, {'2017.12.13': {'ac.set': [101, 100]}});
     });
   });
 });
