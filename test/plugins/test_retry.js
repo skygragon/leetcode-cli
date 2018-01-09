25fa7f7c818396069dda247ac3754dd9d2b9d1ca
Eric Wang <skygragon@gmail.com>
2018-01-09 22:37:15 +0800

    Config max retry.
    Signed-off-by: Eric Wang <skygragon@gmail.com>

--- test/plugins/test_retry.js
+++ test/plugins/test_retry.js
@@ -35,21 +35,37 @@ describe('plugin:retry', function() {
     });
   });
 
-  it('should retry if session expired', function(done) {
+  it('should retry ok if finally ok', function(done) {
     config.autologin.enable = true;
+    config.autologin.retry = 3;
 
     let n = 0;
     NEXT.getProblems = function(cb) {
-      ++n;
-      if (n === 1) return cb(session.errors.EXPIRED);
-      return cb(null, PROBLEMS);
+      return ++n <= 3 ? cb(session.errors.EXPIRED) : cb(null, PROBLEMS);
     };
-
     NEXT.login = (user, cb) => cb(null, user);
 
     plugin.getProblems(function(e, problems) {
-      assert.equal(e, null);
+      assert.notExists(e);
       assert.equal(problems, PROBLEMS);
+      done();
+    });
+  });
+
+  it('should retry fail if always failed', function(done) {
+    config.autologin.enable = true;
+    config.autologin.retry = 2;
+
+    let n = 0;
+    NEXT.getProblems = function(cb) {
+      return ++n <= 3 ? cb(session.errors.EXPIRED) : cb(null, PROBLEMS);
+    };
+    NEXT.login = (user, cb) => {
+      return n == 1 ? cb(null, user) : cb('login failed');
+    }
+
+    plugin.getProblems(function(e) {
+      assert.deepEqual(e, session.errors.EXPIRED);
       done();
     });
   });
@@ -59,15 +75,12 @@ describe('plugin:retry', function() {
 
     let n = 0;
     NEXT.getProblems = function(cb) {
-      ++n;
-      if (n === 1) return cb(session.errors.EXPIRED);
-      return cb(null, PROBLEMS);
+      return ++n === 1 ? cb(session.errors.EXPIRED) : cb(null, PROBLEMS);
     };
-
     session.getUser = () => null;
 
     plugin.getProblems(function(e, problems) {
-      assert.equal(e, null);
+      assert.notExists(e);
       assert.equal(problems, PROBLEMS);
       done();
     });
