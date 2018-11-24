7704e1181650481fe0609aad3eeb954c53c3d25c
Eric Wang <skygragon@gmail.com>
2018-11-24 13:32:42 +0800

    [pkg] bail out if error occurs
    Signed-off-by: Eric Wang <skygragon@gmail.com>

--- bin/pkg.bat
+++ bin/pkg.bat
@@ -3,17 +3,23 @@ set arch=%1
 set os=%2
 set ver=%3
 
+set dist=dist\
+set file=leetcode-cli.node%ver%.%os%.%arch%.zip
+
+mkdir %dist%
+del /q %dist%\*
+del /q *.zip
+
 for %%x in (company cookie.chrome cookie.firefox cpp.lint cpp.run github leetcode.cn lintcode solution.discuss) do (
     echo [%%x]
     node bin\leetcode ext -i %%x
+    if %ERRORLEVEL% gtr 0 exit /b 1
 )
 
-set dist=dist\
-mkdir %dist%
-del /q %dist%\*
-
 for /r . %%x in (*.node) do copy %%x %dist%
-npm run pkg -- node%ver%-%os%-%arch%
+call npm run pkg -- node%ver%-%os%-%arch%
+if %ERRORLEVEL% gtr 0 exit /b 1
 
-set file=leetcode-cli.node%ver%.%os%.%arch%.zip
 7z a %file% %dist%
+if %ERRORLEVEL% gtr 0 exit /b 1
+exit 0
--- bin/pkg.sh
+++ bin/pkg.sh
@@ -4,6 +4,13 @@ arch=$1
 os=$2
 ver=$3
 
+DIST=./dist
+FILE=leetcode-cli.node$ver.$os.$arch.tar.gz
+
+mkdir -p $DIST
+rm -rf $DIST/*
+rm -rf $FILE
+
 plugins="company cookie.chrome cookie.firefox cpp.lint cpp.run github leetcode.cn lintcode solution.discuss"
 
 for plugin in $plugins; do
@@ -11,13 +18,9 @@ for plugin in $plugins; do
   ./bin/leetcode ext -i $plugin
 done
 
-DIST=./dist
-mkdir -p $DIST
-rm -rf $DIST/*
-
 find node_modules -name "*.node" -exec cp {} $DIST \;
 npm run pkg -- node$ver-$os-$arch
 
-FILE=leetcode-cli.node$ver.$os.$arch.tar.gz
-tar zcvf $FILE  $DIST
+tar zcvf $FILE $DIST
 ls -al $FILE
+exit 0
