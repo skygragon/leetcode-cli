a3b384f34ae3dde018af69262a18c92119f3eb6f
Eric Wang <skygragon@gmail.com>
2018-11-24 11:10:43 +0800

    [pkg] refactor script for linux
    Signed-off-by: Eric Wang <skygragon@gmail.com>

---
+++ bin/pkg.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+arch=$1
+os=$2
+ver=$3
+
+plugins="company cookie.chrome cookie.firefox cpp.lint cpp.run github leetcode.cn lintcode solution.discuss"
+
+for plugin in $plugins; do
+  echo "[$plugin]"
+  #./bin/leetcode ext -i $plugin
+done
+
+DIST=./dist
+mkdir -p $DIST
+rm -rf $DIST/*
+
+find node_modules -name "*.node" -exec cp {} $DIST \;
+npm run pkg -- node$ver-$os-$arch
+
+FILE=leetcode-cli.node$ver.$os.$arch.tar.gz
+tar zcvf $FILE  $DIST
+ls -al $FILE
